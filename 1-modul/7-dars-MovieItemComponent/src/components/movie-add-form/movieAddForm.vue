dbebe31884061ab5fe21ecb04d621e657b9e4f68
decoder <abdulbosit19980204@gmail.com>
2023-04-18 23:34:08 +0500

    Movie Item component style changed

---
+++ 1-modul/7-dars-MovieItemComponent/src/components/movie-add-form/movieAddForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="movie-add-form">
+        <h3>Yangi kino qoshish</h3>
+        <form action="" class="add-form d-flex">
+            <input type="text" name="" id="" class="form-control new-movie-label" placeholder="Qanday kino">
+            <input type="text" name="" id="" class="form-control new-movie-label mx-2" placeholder="Nechi marotaba korilgan">
+
+            <button class="btn btn-outline-dark mx-3" type="submit">Qoshish</button>
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style >
+.movie-add-form{
+    margin-top: 30px;
+    padding: 1.5rem;
+    border-radius: 4px;
+    background-color: #fcfcf5;
+    border-radius: 4px;
+    box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);
+}
+    
+</style>
---
+++ 1-modul/7-dars-MovieItemComponent/src/components/movie-list-item/movieListItem.vue
@@ -0,0 +1,103 @@
+<template >
+  <li class="list-group-item d-flex justify-content-between "> 
+    <span class="list-group-item-lable">kino 1</span>
+    <input type="number" class="list-group-item-input" defaultValue="811">
+    <div class="d-flex justify-content-center align-items-center">
+        <button type="button" class="btn-cookie btn-sm">
+            <i class="fas fa-cookie"></i>
+        </button>
+        <button type="button" class="btn-trash btn-sm">
+            <i class="fas fa-trash"></i>
+        </button>
+       
+            <i class="fas fa-star"></i>
+       
+    </div>
+  </li>
+  <li class="list-group-item d-flex justify-content-between favourite"> 
+    <span class="list-group-item-lable">kino 1</span>
+    <input type="number" class="list-group-item-input" defaultValue="811">
+    <div class="d-flex justify-content-center align-items-center">
+        <button type="button" class="btn-cookie btn-sm">
+            <i class="fas fa-cookie"></i>
+        </button>
+        <button type="button" class="btn-trash btn-sm">
+            <i class="fas fa-trash"></i>
+        </button>
+       
+            <i class="fas fa-star"></i>
+       
+    </div>
+  </li>
+</template>
+<script>
+export default {
+   
+}
+</script>
+<style scoped >
+   .list-group-item{
+    padding: 15px 20px;
+    border: none;
+    border-bottom: 1px solid #3d5a80;
+
+   }
+   .list-group-item:last-child{
+    border-bottom: none;
+   }
+
+   .list-group-item span{
+    line-height: 35px;
+    font-size: 20px;
+    cursor: pointer;
+    width: 550px;   
+   }
+
+   .list-group-item input{
+    line-height: 35px;
+    font-size: 20px;
+    text-align: center;
+    border: none;
+    outline: none;
+   }
+
+   .list-group-item button{
+    width: 35px;
+    height: 35px;
+    margin: 3px;
+    border: none;
+    font-size: 17px;
+    cursor: pointer;
+    border-radius: 5px;
+    background-color: aliceblue;
+   }
+
+   .list-group-item  .btn-cookie{
+    color: #e09f3e;
+   }
+   .list-group-item .btn-trash{
+    color: #e5383b;
+   }
+   .list-group-item .fa-star{
+    width: 35px;
+    height: 35px;
+    text-align: center;
+    line-height: 35px;
+    font-size: 16px;
+    color: #ffd700;
+    transition: 0.3s all;
+    transform: translateX(30px);
+    opacity: 0  ;
+   }
+   .list-group-item.like .fa-star{
+opacity: 1;
+transform: translateX(0);
+   }
+
+   .list-group-item.favourite .list-group-item-lable{
+    color: #e09f3e;
+   }
+   .list-group-item.favourite .list-group-item-input{
+    color: #e09f3e;
+   }
+</style>
