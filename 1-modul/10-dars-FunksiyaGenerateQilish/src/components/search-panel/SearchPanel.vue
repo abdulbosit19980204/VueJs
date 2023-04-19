cb4cf21922880b1e11cbbf2bfda91bb4b6ab73e9
decoder <abdulbosit19980204@gmail.com>
2023-04-20 02:14:24 +0500

    10 - dars Funksiya generate qilish Formalar bilan ishlash

---
+++ 1-modul/10-dars-FunksiyaGenerateQilish/src/components/search-panel/SearchPanel.vue
@@ -0,0 +1,19 @@
+<template lang="">
+    <div class="container">
+       <input type="text" class="form-control search-input" placeholder="Kinolarni qidirish"/>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    .search-input{
+    padding: 1.5rem;
+    border-radius: 4px;
+    background-color: #fcfcf5;
+    border-radius: 4px;
+    box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);
+    }
+</style>
