8e7ce74a2e6f112f6b9902ec6ffbd5a6893f4536
decoder <abdulbosit19980204@gmail.com>
2023-04-25 04:34:59 +0500

    Button optimization

---
+++ 1-modul/17-dars-ButtonOptimization/src/components/app-filter/appFilter.vue
@@ -0,0 +1,35 @@
+<template lang="">
+<div class="btn-group mt-3">
+    <button class="btn " type='button' :class="[filterName=='all' ? 'btn-dark' : 'btn-outline-dark' ]" @click ='filterHandler("all")'>Barcha kinolar</button>
+    <button class="btn " type='button' :class="[filterName=='popular' ? 'btn-dark' : 'btn-outline-dark' ]" @click ='filterHandler("popular")'>Mashxur kinolar</button>
+    <button class="btn " type='button' :class="[filterName=='mostViewers' ? 'btn-dark' : 'btn-outline-dark' ]" @click ='filterHandler("mostViewers")'>Eng kop korilgan kinolar</button>
+</div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            filter:'all',
+        }
+    },
+    props:{
+        updateFilterHandler:{
+            type: Function,
+            required: true,
+        },
+        filterName:{
+            tyoe: String,
+            required: true,
+        },
+    },
+    methods: {
+        filterHandler(filter){
+            this.filter = filter
+            this.updateFilterHandler(this.filter)
+        }
+    },
+}
+</script>
+<style scoped>
+    
+</style>
