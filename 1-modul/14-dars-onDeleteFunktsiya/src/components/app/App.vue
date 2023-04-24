7a064ef5e3fc4c28cc516b933b6168adb59409ec
decoder <abdulbosit19980204@gmail.com>
2023-04-24 05:09:47 +0500

    OnDelete funksiya

---
+++ 1-modul/14-dars-onDeleteFunktsiya/src/components/app/App.vue
@@ -0,0 +1,112 @@
+<template >
+  <div class="container mt-3">
+    <div class="app font-monospace">
+      <div class="content">
+        <AppInfo :allMoviesCount="movies.length" :favouriteMoviesCount="movies.filter(c=>c.favourite).length"/>
+        <div class="search-panel">
+          <SearchPanel/>
+          <AppFilter/>
+        </div>
+        <MovieList :movies="movies" @onToggle="onToggleHandler" />
+        <MovieAddForm @createMovie="createMovie" />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import AppInfo from "@/components/app-info/appInfo.vue"
+import SearchPanel from "@/components/search-panel/SearchPanel.vue"
+import AppFilter from "@/components/app-filter/appFilter.vue"
+import MovieList from "@/components/movie-list/movieList.vue"
+import MovieAddForm from "@/components/movie-add-form/movieAddForm.vue"
+
+export default {
+  components:{AppInfo,SearchPanel,AppFilter,MovieList,MovieAddForm},
+    data(){
+        return {
+            movies:[
+                {
+                name: "Umar",
+                viewers:505,
+                favourite: true,
+                like: true,
+                id:1
+            },
+            {
+                name: "Usmon New Film",
+                viewers:525,
+                favourite: false,
+                like: true,
+                id:2
+            },
+            {
+                name: "Kino1",
+                viewers:75,
+                favourite: true,
+                like: true,
+                id:3
+            },
+            {
+                name: "Film1",
+                viewers:55,
+                favourite: true,
+                like: false,
+                id:4,
+            },
+            {
+                name: "Film2",
+                viewers:155,
+                favourite: false,
+                like: false,
+                id:5,
+            },
+            {
+                name: "Film3",
+                viewers:775,
+                favourite: true,
+                like: false,
+                id:6
+            },
+        ],
+        }
+    },
+
+    methods:{
+      createMovie(item){  
+      this.movies.push(item)
+     },
+     onToggleHandler({id, prop}){
+       
+     this.movies = this.movies.map(item=>{
+        if (item.id==id) {
+         return {...item, [prop]: !item[prop]}
+        }return item
+      })
+      
+      
+     },
+  
+    }
+    
+}
+</script>
+<style>
+  .app{
+    height: 100vh;
+    color: black;
+  }
+  .content{
+    width: 1000px;
+    min-height: 700px;
+    background-color: white;
+    margin: 0 auto;
+    padding: 5rem 0;
+  }
+  .search-panel{
+    margin-top: 2rem;
+    padding: 1.5rem;
+    background-color: #fcfcf5;
+    border-radius: 4px;
+    box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);
+  }
+</style>
---
+++ 1-modul/14-dars-onDeleteFunktsiya/src/components/movie-list/movieList.vue
@@ -0,0 +1,33 @@
+<template>
+    <ul class="movie-list list-group" >
+        <h3>Movie List</h3>
+        <MovieListItem v-for="movie in movies" v-bind:movie="movie" :key="movie.id" @onToggle="$emit('onToggle', $event)" />
+           
+    </ul>
+</template>
+<script>
+
+import MovieListItem from "@/components/movie-list-item/movieListItem.vue"
+export default {
+    components:{
+        MovieListItem,
+    },
+    props:{
+        movies:{
+            type: Array,
+            required:true
+        }
+    }
+}
+</script>
+<style scoped>
+
+    .movie-list{
+    margin-top: 25px;
+    padding: 1.5rem;
+    border-radius: 4px;
+    background-color: #fcfcf5;
+    border-radius: 4px;
+    box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);
+    }
+</style>
