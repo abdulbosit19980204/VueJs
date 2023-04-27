cbd67897e309db94b75f9977424777ae81b2ea3c
decoder <abdulbosit19980204@gmail.com>
2023-04-28 04:14:22 +0500

    Lifecycle methods

---
+++ 2-modul/20-dars-LifecycleMethods/src/components/nazarya.vue
@@ -0,0 +1,18 @@
+<template >
+    <div>
+           <h1>Lifecycle method</h1>
+           <ol>
+           <li>mounted</li>
+           <li>updated</li>
+           <li>unounted</li>
+           </ol> 
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style >
+    
+</style>
