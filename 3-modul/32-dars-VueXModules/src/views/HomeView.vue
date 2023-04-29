8b12501875e4724d20a939220fa64df9d2ce83ee
decoder <abdulbosit19980204@gmail.com>
2023-04-30 02:19:18 +0500

    VueX modules

---
+++ 3-modul/32-dars-VueXModules/src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template >
+    <div class="fs-3">
+        <h1>Counter {{ counter }}</h1>
+        <div class="w-25">
+                <Button @click="increaseHandler"><i class="fas fa-plus"></i> Increase</Button>
+                <Button @click="decreaseHandler"><i class="fas fa-minus"></i> Decrease</Button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    methods:{
+        increaseHandler(){
+            this.$store.commit('increment')
+            
+        },
+        decreaseHandler(){
+            this.$store.commit('decrement')
+        }
+    },
+    computed:{
+        counter(){
+            return this.$store.state.count
+        }
+    }
+}
+</script>
+<style >
+    
+</style>
