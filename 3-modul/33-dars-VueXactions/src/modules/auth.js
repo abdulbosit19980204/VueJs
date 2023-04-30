555079df2a240a77884c2b9f606458c596d92004
decoder <abdulbosit19980204@gmail.com>
2023-04-30 18:01:50 +0500

    VueX actions

--- 3-modul/33-dars-VueXactions/src/modules/auth.js
+++ 3-modul/33-dars-VueXactions/src/modules/auth.js
@@ -6,4 +6,11 @@ const mutations = {
         state.isLoading = true
     }
 }
-export default { state, mutations }
+const actions = {
+    register(context) {
+        setTimeout(() => {
+            context.commit('setLoading')
+        }, 2000);
+    }
+}
+export default { state, mutations, actions }
