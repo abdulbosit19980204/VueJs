c96c4b9d530710034a60d7fcd43ed2b1ca20d42d
decoder <abdulbosit19980204@gmail.com>
2023-04-30 18:03:44 +0500

    Service Constructor

---
+++ 3-modul/34-dars-ServiceConstructor/src/modules/auth.js
@@ -0,0 +1,16 @@
+const state = {
+    isLoading: false
+}
+const mutations = {
+    setLoading(state) {
+        state.isLoading = true
+    }
+}
+const actions = {
+    register(context) {
+        setTimeout(() => {
+            context.commit('setLoading')
+        }, 2000);
+    }
+}
+export default { state, mutations, actions }
