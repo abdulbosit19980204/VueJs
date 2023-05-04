34f7b679c0f01de91eb71508dfe1ff2d6ffb9ea2
decoder <abdulbosit19980204@gmail.com>
2023-05-04 17:47:38 +0500

    Token bilan ishlash

---
+++ 3-modul/37-dars-TokenBilanIshlash/src/components/Login.vue
@@ -0,0 +1,22 @@
+<template >
+  <div class="container text-center ">
+    <main class="form-signin w-25 m-auto mt-5">
+      <form>
+        <img class="mb-4" src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" width="72" >
+        <h1 class="h3 mb-3 fw-normal mt-3">Please sign in</h1>
+        <Input :label="'Email'" :type="email"  ></Input>
+        <Input :label="'Password'" :type="password"  ></Input>
+              
+        <Button type="submit">Login</Button>
+      </form>
+    </main>
+  </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style >
+    
+</style>
