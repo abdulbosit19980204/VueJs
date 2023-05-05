96c9fe7131713ab89fcbd3afdf60252cb3fc8308
decoder <abdulbosit19980204@gmail.com>
2023-05-05 10:21:38 +0500

    Validation Error

---
+++ 3-modul/38-dars-ValidationError/src/components/Register.vue
@@ -0,0 +1,45 @@
+<template >
+    <div class="container text-center ">
+        <main class="form-signin w-25 m-auto mt-5">
+            <form>
+                <img class="mb-4" src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" width="72" height="72">
+                <h1 class="h3 mb-3 fw-normal mt-3">Register</h1>
+
+                <Input :label="'Name'" :type="text"            v-model="username" ></Input>
+                <Input :label="'Email address'" :type="email"  v-model="email" ></Input>
+                <Input :label="'Password'" :type="password"    v-model="password" ></Input>
+                
+                <Button type="submit" :disabled="isLoading" @click="submitHandler">Register</Button>
+              </form>
+        </main>
+    </div>
+   
+</template>
+<script>
+export default {
+    computed:{
+        isLoading(){
+            return this.$store.state.auth.isLoading
+        }
+    },
+    methods:{
+        submitHandler(e){
+            e.preventDefault();
+            const data ={
+                email:this.email,
+                password:this.password,
+                username:this.username,
+            }
+            this.$store.dispatch('register',data).then((result) => {
+                console.log('USER',result)
+                this.$router.push({name:'home'})
+            }).catch((err) => {
+                console.log('Error',err);
+            });
+        }
+    }
+}
+</script>
+<style >
+    
+</style>
