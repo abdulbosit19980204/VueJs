26093401d33545fe4a8e555a9ad1e4c25826bff6
decoder <abdulbosit19980204@gmail.com>
2023-05-11 19:30:27 +0500

    Article API

---
+++ 3-modul/44-dars-ArticlesAPI/src/helpers/persistaneStorage.js
@@ -0,0 +1,23 @@
+export const getItem = key => {
+    try {
+        return JSON.parse(localStorage.getItem(key))
+    } catch (error) {
+        console.log("Error getting data => ", error.message);
+        return null
+    }
+}
+export const setItem = (key, data) => {
+    try {
+        localStorage.setItem(key, JSON.stringify(data))
+    } catch (error) {
+        console.log("Error saving data", error.message);
+    }
+}
+
+export const removeItem = (key) => {
+    try {
+        localStorage.removeItem(key)
+    } catch (error) {
+        console.log("Error deleting data");
+    }
+}
