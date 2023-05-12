608fef836c1f339e2d3ac9d517d1f364220923c0
decoder <abdulbosit19980204@gmail.com>
2023-05-12 23:54:48 +0500

    Loader

---
+++ 3-modul/46-dars-Loader/src/components/ArticleCard.vue
@@ -0,0 +1,36 @@
+<template lang="">
+        <div class="col">
+          <div class="card shadow-sm">
+            <img src="https://texts.com/ogcover.png" alt="" class="bd-placeholder-img card-img-top" >
+         
+           <div class="card-body">
+           <div class="text-success btn-group">
+              <small class="my-0 btn btn-outline-light  active">{{article.author.username}}</small>
+              <small class="text-warning btn btn-outline-light ">{{article.updatedAt}}</small>
+              </div>
+              <p class="card-title fw-bold">{{article.title}}</p>
+              <p class="card-text">{{article.body}}</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-body-secondary">{{article.createdAt}}</small>
+               </div>
+            </div>
+          </div>
+        </div>
+</template>
+<script>
+export default {
+    props:{
+      article:{
+        type: Object,
+        required: true,
+      }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ 3-modul/46-dars-Loader/src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template >
+    <div class="fs-6" >
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <ArticleCard v-for='article in data' :article='article' :key="article.id"></ArticleCard>
+    </div>
+     </div>
+</template>
+<script>
+import { mapState } from "vuex";
+import  ArticleCard  from "@/components/ArticleCard.vue";
+export default {
+  components:{ArticleCard,},
+     computed:{
+    ...mapState({
+      data: state => state.articles.data,
+      isLoading:state=> state.articles.isLoading,
+      error: state=> state.articles.error,
+    }),
+  },
+
+  mounted() {
+    this.$store.dispatch('articles')
+  },
+
+}
+</script>
+<style >
+    
+</style>
