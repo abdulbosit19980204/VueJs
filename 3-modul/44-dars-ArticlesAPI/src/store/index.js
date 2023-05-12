9b81ce2ae058ee68dd40868acc5a3ed4ed24eb99
decoder <abdulbosit19980204@gmail.com>
2023-05-12 22:45:24 +0500

    Articles API

--- 3-modul/44-dars-ArticlesAPI/src/store/index.js
+++ 3-modul/44-dars-ArticlesAPI/src/store/index.js
@@ -1,11 +1,12 @@
 import { createStore } from 'vuex'
 import auth from '../modules/auth'
+import articles from "../modules/articles";
 // Create a new store instance.
 const store = createStore({
     state: {},
     mutations: {},
     actions: {},
-    modules: { auth },
+    modules: { auth, articles },
 })
 
 export default store
