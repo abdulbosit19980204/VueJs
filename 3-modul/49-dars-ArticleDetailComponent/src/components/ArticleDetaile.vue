8a14f443a1f91b0749b3e60736685eb216500923
decoder <abdulbosit19980204@gmail.com>
2023-05-13 23:10:58 +0500

    Article Detaile component

---
+++ 3-modul/49-dars-ArticleDetailComponent/src/components/ArticleDetaile.vue
@@ -0,0 +1,18 @@
+<template >
+    <div>
+        <h1>Article Detaile</h1>
+        {{article}}
+    </div>
+  
+</template>
+<script>
+import { mapState } from "vuex";
+export default {
+    computed:{
+        ...mapState({ article: state => state.articles.articleDetail })
+    }
+}
+</script>
+<style >
+    
+</style>
