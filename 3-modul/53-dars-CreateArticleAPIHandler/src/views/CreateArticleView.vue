e9f84d32a617319a8e28704a1bc23f5f8dc720bf
decoder <abdulbosit19980204@gmail.com>
2023-05-15 00:37:26 +0500

    Create Article API Handler

---
+++ 3-modul/53-dars-CreateArticleAPIHandler/src/views/CreateArticleView.vue
@@ -0,0 +1,37 @@
+<template >
+        <p class="text-center display-2">Create article</p>
+    <div class="w-50 mx-auto">
+    <form @submit.prevent>
+        <Input type='text' label='title' v-model="title"/>
+        <TextArea type='text' label='description' v-model="description"/>
+        <TextArea type='text' label='Body' v-model="body"/>
+        <Button @click="CreateArticleHandler">Create article</Button>
+    </form>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+                title:'',   
+                description:'',
+                body: '',
+            
+        }
+    },
+    methods:{
+        CreateArticleHandler(){
+            const article ={
+                title: this.title,
+                description: this.description,
+                body: this.body,
+                tagList: [],
+            }
+            console.log(article);
+        },
+    }
+}
+</script>
+<style >
+    
+</style>
---
+++ 3-modul/53-dars-CreateArticleAPIHandler/src/views/index.js
@@ -0,0 +1,14 @@
+export { default as HomeView }
+from './HomeView.vue'
+
+export { default as RegisterView }
+from './RegisterView.vue'
+
+export { default as LoginView }
+from './LoginView.vue'
+
+export { default as ArticleDetailView }
+from './ArticleDetailView.vue'
+
+export { default as CreateArticleView }
+from './CreateArticleView.vue'
